99215a49826cb6523a0b89b431c1e14f98334f4c
codeandesarrollo <codean.desarrollo@gmail.com>
2025-03-28 14:58:51 -0600

    Add files via upload

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const stripe = require('stripe')('sk_live_...'); // Tu clave secreta
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'prod_S1TfHiN3Itp6c6', // ID de precio de tu producto
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      // Define la URL de éxito con el placeholder de la sesión
+      success_url: 'https://ebookscodean.vercel.app/exito.html?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'https://ebookscodean.vercel.app/cancel',
+    });
+    res.json({ id: session.id });
+  } catch (error) {
+    res.status(500).send('Error creando la sesión de Checkout');
+  }
+});
