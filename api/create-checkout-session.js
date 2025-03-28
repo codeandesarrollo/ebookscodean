e924c0cd6748d53cb5f091d2db249c77e0a39f87
codeandesarrollo <codean.desarrollo@gmail.com>
2025-03-28 16:22:43 -0600

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.JWT_SECRET);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
